280858a54e435e0756349fce64040c678f8844f0
Onur Alp Akin <onralpakin@gmail.com>
2022-07-13 15:36:59 +0300

    prettify startup message

--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 // import express from 'express';
 
+const chalk = require('chalk');
 const config = require('./config');
 const app = require('./server/app');
 const router = require('./server/router');
 
 const port = process.env.PORT || 3000;
 
-
-console.log(config)
 app.use(config.api.endpoint, router); // use the api router for all requests to the api endpoint
 
 // start the server
 app.listen(port, () => {
-    console.log(`eksisozluk-api running on http://localhost:${port}`);
+    //.....
+    console.log(chalk.yellow('..................................'));
+    console.log(chalk.green(`=====[ ${config.api.name} ]=====`));
+    console.log(chalk.green(`Port:\t\t\t${config.api.port}`));
+    console.log(chalk.green(`API Endpoint:\t\t${config.api.endpoint}`));
+    console.log(chalk.green("\ndatabase connection is established"));
+    console.log(chalk.yellow('..................................'));
+    console.log(chalk.blue(`eksisozluk-api running on http://localhost:${port}`));
 });
