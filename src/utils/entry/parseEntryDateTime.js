ae090e535e0a11413d47fc1e06bb4676f79ec37e
Onur Alp Akin <onralpakin@gmail.com>
2022-07-13 08:10:20 +0300

    add return cond

--- src/utils/entry/parseEntryDateTime.js
+++ src/utils/entry/parseEntryDateTime.js
@@ -2,9 +2,15 @@ module.exports = (date) => {
     // parse the date from given string
     // format: "17.06.2022 23:58 ~ 18.06.2022 00:03"
 
+    // eger sadece birisi varsa oteki gozukmuyor updated de
     const createdAt = date;
     const updatedAt = createdAt.includes("~") ? createdAt.split(" ~ ")[1].trim() : null;
     const [createdAtDate, createdAtTime] = createdAt.split(" ");
     const [updatedAtDate, updatedAtTime] = updatedAt ? updatedAt.split(" ") : [null, null];
-    return [createdAtDate, createdAtTime, updatedAtDate, updatedAtTime];
+    return [
+        createdAtDate, 
+        createdAtTime, 
+        updatedAtDate ? updatedAtDate : null, 
+        updatedAtTime ? updatedAtTime : null
+    ];
 }
