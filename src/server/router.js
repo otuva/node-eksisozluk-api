ef085a3b956a1b753fec7c3760bc3cb8c36d602e
Onur Alp Akin <onralpakin@gmail.com>
2022-07-13 08:10:03 +0300

    change router functions to arrow func

--- src/server/router.js
+++ src/server/router.js
@@ -3,6 +3,7 @@ const express = require('express');
 const entry = require('../controllers/entry');
 const debe = require('../controllers/debe');
 const user = require('../controllers/user');
+const page = require('../controllers/page');
 // const search = require('../controllers/search');
 
 const router = express.Router();
@@ -11,46 +12,52 @@ let response;
 
 
 /* GET threads listing. */
-// router.get('/basliklar', async function(req, res, next) {
+// router.get('/basliklar', async (req, res, next) => {
 //   response = await thread.list(req.url);
 //   res.json(response);
 // });
 
 /* GET single thread */
-// router.get('/baslik/:slug', async function(req, res, next) {
+// router.get('/baslik/:slug', async (req, res, next) => {
 //   response = await thread.detail(req.url);
 //   res.json(response);
 // });
 
 /* GET single entry */
-router.get('/entry/:id', async function (req, res, next) {
+router.get('/entry/:id', async (req, res, next) => {
     // console.log(req.params)
     response = await entry(req.params.id);
     res.json(response);
 });
 
 /* GET debe entries listing. */
-router.get('/debe', async function(req, res, next) {
+router.get('/debe', async (req, res, next) => {
   response = await debe();
   res.json(response);
 });
 
 /* GET user profile */
-router.get('/biri/:nick', async function(req, res, next) {
+router.get('/biri/:nick', async (req, res, next) => {
   response = await user(req.params.nick);
   res.json(response);
 });
 
 /* GET search result */
-// router.get('/ara/:query', async function(req, res, next) {
+// router.get('/ara/:query', async (req, res, next) => {
 //   response = await search.getSearch(req.url, req.params.query);
 //   res.json(response);
 // });
 
 /* GET autocomplete search result */
-// router.get('/autocomplete/:query', async function(req, res, next) {
+// router.get('/autocomplete/:query', async (req, res, next) => {
 //   response = await search.autoComplete(req.params.query);
 //   res.json(response);
 // });
+
+/* GET user entry */
+router.get('/son-entryleri/:nick/:page', async (req, res, next) => {
+  response = await page(req.params.nick,req.params.page);
+  res.json(response);
+});
 
 module.exports = router;
