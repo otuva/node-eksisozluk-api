d566111293002ee2fa9aca5234e782de0df434c7
Onur Alp Akin <onralpakin@gmail.com>
2022-07-14 18:36:40 +0300

    add new options

--- src/config.js
+++ src/config.js
@@ -4,24 +4,68 @@ const api = {
   name: 'eksisozluk-api',
   endpoint: '/api',
   port: 3000,
+  limiting: {
+    enabled: false,
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+  }
+}
+
+const doc = {
+  endpoint: '/doc',
+  enabled: true,
 }
 
 const user = {
-  autoRetrieveLastEntries : true, // causes two requests instead of one (mimics the behavior of the website)
+  autoRetrieveLastEntries: true, // causes two requests instead of one (mimics the behavior of the website)
 }
 
-const asyncRequestHeaders = {"X-Requested-With": "XMLHttpRequest"}
+const topic = {
+  // for easier configuration
+  enabledChannels: [
+    "haber",
+    "sinema",
+    "bilim",
+    "eğitim",
+    "spoiler",
+    "müzik",
+    "edebiyat",
+    "ekonomi",
+    "tarih",
+    "yeme-içme",
+    "ilişkiler",
+    "siyaset",
+    "teknoloji",
+    "sanat",
+    "moda",
+    "otomotiv",
+    "magazin",
+    "ekşi-sözlük",
+    "spor",
+    "motosiklet",
+    "sağlık",
+    "oyun",
+    "anket",
+    "programlama",
+    "tv",
+    "seyahat",
+    "havacılık",
+    "troll"
+  ],
+  enabledSubTopics: [
+    "gundem",
+    "sorunsal",
+    "tarihte-bugun",
+    "basiboslar"
+  ],
+}
+
+const asyncRequestHeaders = { "X-Requested-With": "XMLHttpRequest" }
 
 module.exports = {
   api,
+  doc,
   user,
+  topic,
   asyncRequestHeaders
 };
-// export const apiEnpoint = '/api';
-// export const serveDoc = true;
-// export const serveClientEx = true;
-// export const docEndpoint = '/doc';
-// export const clientEndpoint = '/';
-// export const useMongo = process.env.USE_MONGO || false;
-// export const cacheAPI = process.env.CACHE_API || false;
-// export const limitAPI = process.env.LIMIT_API || false;
