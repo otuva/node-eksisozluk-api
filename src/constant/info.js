9fb5213932a1f12bbfda051eeca96fb6e7740fce
Onur Alp Akin <onralpakin@gmail.com>
2022-07-15 09:29:34 +0300

    add info consts

---
+++ src/constant/info.js
@@ -0,0 +1,8 @@
+const INFO = {
+    TOPIC: {
+        REDIRECTING: "Topic found but redirecting to the full path",
+        REDIRECTING_DISABLED: "Topic found but it's not in the form of full slug. Redirecting is disabled so page parameter won't be applied. To enable redirecting, please change config.topic.allowRedirect to true.",
+    }
+}
+
+module.exports = INFO;
