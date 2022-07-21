fae2b898cdb14bb8f8dce5d6d0b512e023374122
Onur Alp Akin <onralpakin@gmail.com>
2022-07-21 22:08:59 +0300

    handle empty arrays

--- src/controllers/autoComplete.js
+++ src/controllers/autoComplete.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const urls = require('../constant/urls');
+const errors = require('../constant/errors');
 const config = require('../config');
 
 module.exports = async (query) => {
@@ -9,6 +10,10 @@ module.exports = async (query) => {
             { headers: config.asyncRequestHeaders });
     } catch (err) {
         return { error: err.message };
+    }
+
+    if (response.data.Titles.length === 0 && response.data.Nicks.length === 0) {
+        return { error: errors.SEARCH.NOT_FOUND};
     }
     return response.data;
 };
