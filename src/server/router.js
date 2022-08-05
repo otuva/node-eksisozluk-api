2942fcd902ee333a327f6cfaccff669e0335e49d
Onur Alp Akin <onralpakin@gmail.com>
2022-08-05 11:27:37 +0300

    change EPs

--- src/server/router.js
+++ src/server/router.js
@@ -1,32 +1,32 @@
 const express = require('express');
-const topic = require('../controllers/topic');
-const topicList = require('../controllers/topicList');
-const entry = require('../controllers/entry');
-const debe = require('../controllers/debe');
-const user = require('../controllers/user');
-const page = require('../controllers/page');
-const search = require('../controllers/search');
-const autoComplete = require('../controllers/autoComplete');
-const allChannels = require('../controllers/allChannels');
+const topic = require('../controllers/topic/topic');
+const topicList = require('../controllers/topic/topicList');
+const entry = require('../controllers/entry/entry');
+const debe = require('../controllers/index/debe');
+const user = require('../controllers/user/user');
+const page = require('../controllers/user/page');
+const search = require('../controllers/index/search');
+const autoComplete = require('../controllers/index/autoComplete');
+const allChannels = require('../controllers/index/allChannels');
 
 const router = express.Router();
 
 let response;
 
 /* GET topic channel listing. */
-router.get('/basliklar/kanal/:choice/:page?', async (req, res, next) => {
+router.get('/topic/list/channel/:choice/:page?', async (req, res, next) => {
   response = await topicList(req.params.choice, req.params.page, true);
   res.json(response);
 });
 
 /* GET topic listing. */
-router.get('/basliklar/:choice/:page?', async (req, res, next) => {
+router.get('/topic/list/:choice/:page?', async (req, res, next) => {
   response = await topicList(req.params.choice, req.params.page);
   res.json(response);
 });
 
 /* GET single thread */
-router.get('/baslik/:slug/:page?', async (req, res, next) => {
+router.get('/topic/:slug/:page?', async (req, res, next) => {
   response = await topic(req.params.slug, req.params.page);
   if (response.redirect) {
     res.redirect(response.redirect);
@@ -43,37 +43,37 @@ router.get('/entry/:id', async (req, res, next) => {
 });
 
 /* GET debe entries listing. */
-router.get('/debe', async (req, res, next) => {
+router.get('/index/debe', async (req, res, next) => {
   response = await debe();
   res.json(response);
 });
 
 /* GET user profile */
-router.get('/biri/:nick', async (req, res, next) => {
+router.get('/user/:nick', async (req, res, next) => {
   response = await user(req.params.nick);
   res.json(response);
 });
 
 /* GET user entry */
-router.get('/son-entryleri/:nick/:page?', async (req, res, next) => {
+router.get('/user/:nick/entries/:page?', async (req, res, next) => {
   response = await page(req.params.nick,req.params.page);
   res.json(response);
 });
 
 /* GET search result */
-router.get('/ara/:query/:page?', async (req, res, next) => {
+router.get('/index/search/:query/:page?', async (req, res, next) => {
   response = await search(req.params.query, req.params.page);
   res.json(response);
 });
 
 /* GET autocomplete search result */
-router.get('/autocomplete/:query', async (req, res, next) => {
+router.get('/index/autocomplete/:query', async (req, res, next) => {
   response = await autoComplete(req.params.query);
   res.json(response);
 });
 
 /* GET all channels */
-router.get('/kanallar', async (req, res, next) => {
+router.get('/index/channels', async (req, res, next) => {
   response = await allChannels();
   res.json(response);
 });
