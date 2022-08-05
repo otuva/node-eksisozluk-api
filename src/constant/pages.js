152a3f1e97fb88dc1f39436072391507d80c33a9
Onur Alp Akin <onralpakin@gmail.com>
2022-08-05 13:07:27 +0300

    add valid user pages

---
+++ src/constant/pages.js
@@ -0,0 +1,11 @@
+const PAGES = {
+    'entries': 'son-entryleri',
+    'favorites': 'favori-entryleri',
+    'most-favorited': 'en-cok-favorilenen-entryleri',
+    'last-voted': 'son-oylananlari',
+    'weekly': 'bu-hafta-dikkat-cekenleri',
+    'handicraft': 'el-emegi-goz-nuru',
+    'most-liked': 'en-begenilenleri',
+}
+
+module.exports = PAGES;
