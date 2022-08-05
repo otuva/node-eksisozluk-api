de03ce5ac874d5961cef622b7a622e3b2a4794ef
Onur Alp Akin <onralpakin@gmail.com>
2022-08-05 13:48:39 +0300

    refactor constant object

--- src/constant/pages.js
+++ src/constant/pages.js
@@ -1,11 +1,22 @@
+// objects for specifically user sections 
 const PAGES = {
-    'entries': 'son-entryleri',
-    'favorites': 'favori-entryleri',
-    'most-favorited': 'en-cok-favorilenen-entryleri',
-    'last-voted': 'son-oylananlari',
-    'weekly': 'bu-hafta-dikkat-cekenleri',
-    'handicraft': 'el-emegi-goz-nuru',
-    'most-liked': 'en-begenilenleri',
+    ENTRY_PAGES: {
+        'entries': 'son-entryleri',
+        'favorites': 'favori-entryleri',
+        'most-favorited': 'en-cok-favorilenen-entryleri',
+        'last-voted': 'son-oylananlari',
+        'weekly': 'bu-hafta-dikkat-cekenleri',
+        'handicraft': 'el-emegi-goz-nuru',
+        'most-liked': 'en-begenilenleri',
+    },
+    NON_ENTRY_PAGES: {
+        'images': 'gorselleri',
+        'favorite-authors': 'favori-yazarlari',
+        'contributed-channels': 'katkida-bulundugu-kanallar',
+        'unfulfilled': 'ukteleri',
+        'questions': 'sorunsalları',
+        'answers': 'sorunsal-yanıtları',
+    }
 }
 
 module.exports = PAGES;
